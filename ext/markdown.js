12b23d798514ee9bf11e4def32ba02ccfa19f762
JixunMoe <Jixun.Moe@Gmail.Com>
2016-08-21 20:43:26 +0100

    Markdown in client will try to trim white space between tags.

--- ext/markdown.js
+++ ext/markdown.js
@@ -2,7 +2,8 @@ function fixMarkdown(html) {
   let result = html;
   result = result.replace(/<table/g, '<table class="table table-hover table-striped table-inline table-bordered"');
   result = result.replace(/<a (href="(https?|\/\/))/g, '<a rel="external nofollow noreferrer" target="_blank" $1');
-  return result;
+  result = result.replace(/>\s+</g, '><');
+  return result.trim();
 }
 
 module.exports = fixMarkdown;
